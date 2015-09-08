3ca6818619c9819e6dbb2811cea13c3d2a9a3e4c
Wilmer Reyes Alfaro <wil@wmpulsistem26>
2015-09-08 17:50:51 -0500

    configuring thymeleaf

--- src/main/java/com/raw/kamaq/HomeController.java
+++ src/main/java/com/raw/kamaq/HomeController.java
@@ -4,17 +4,12 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.raw.kamaq.model.User;
 
 /**
  * Handles requests for the application home page.
@@ -36,23 +31,9 @@ public class HomeController {
 
 		String formattedDate = dateFormat.format(date);
 
-		User user = new User();
-		user.setName("Wil");
-
-		Configuration config = new Configuration().configure();
-		SessionFactory sessionFactory = config.buildSessionFactory();
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-
-		session.save(user);
-
-		session.getTransaction().commit();
-		session.close();
-		sessionFactory.close();
-
 		model.addAttribute("serverTime", formattedDate);
 
-		return "kamaq home";
+		return "welcome";
 	}
 
 }
