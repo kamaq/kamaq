9406f364c8553cd400efc5027c823488532ea1d5
Wilmer Reyes Alfaro <wil@wmpulsistem26>
2015-09-18 17:54:04 -0500

    adding submit login

--- src/main/java/com/raw/kamaq/HomeController.java
+++ src/main/java/com/raw/kamaq/HomeController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -34,6 +36,14 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate);
 
 		return "welcome";
+	}
+
+	@RequestMapping(value = "/submitLoginForm.html", method = RequestMethod.POST)
+	public ModelAndView submitAdmissionController(@RequestParam(value = "username") String username,
+			@RequestParam("password") String password) {
+		ModelAndView model = new ModelAndView("main");
+		model.addObject("msg", "user: " + username + " pass:" + password);
+		return model;
 	}
 
 }
